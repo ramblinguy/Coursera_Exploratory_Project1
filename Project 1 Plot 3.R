d257c687ce847323a7e8cbc1f70876ddd862a250
ramblinguy <ycdong93@gmail.com>
2017-05-23 17:35:13 -0400

    Add files via upload

--- Project 1 Plot 3.R
+++ Project 1 Plot 3.R
@@ -3,3 +3,6 @@ plot(power_final$DateTime,power_final$Sub_metering_1, type="l",col="black",xlab=
 lines(power_final$DateTime,power_final$Sub_metering_2,col="red")
 lines(power_final$DateTime,power_final$Sub_metering_3,col="blue")
 legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_Metering_3"),col=c('black','red','blue'),lty='solid')
+
+dev.copy(png, file="Project 1 Plot 3.png")
+dev.off()
--- Project 1 Plot 4.R
+++ Project 1 Plot 4.R
@@ -10,3 +10,6 @@ lines(power_final$DateTime,power_final$Sub_metering_3,col="blue")
 legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_Metering_3"),col=c('black','red','blue'),lty='solid')
 
 plot(power_final$DateTime,power_final$Global_reactive_power,xlab = 'datetime',ylab = 'Global_reactive_power',col='black',type = "l")
+
+dev.copy(png, file="Project 1 Plot 4.png")
+dev.off()
